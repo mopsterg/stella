08b0a9a5a224159466ff8db0e205f32e679e2bf2
Anupom Ghosh <ubuntu@ip-172-31-40-218.ec2.internal>
2014-11-11 16:08:46 +0000

    default_spec.rb

---
+++ default_spec.rb
@@ -0,0 +1,11 @@
+require 'chefspec'
+
+at_exit { ChefSpec::Coverage.report!}
+describe 'apache-test::default' do
+let(:chef_run) = {ChefSpec::Runner.new.converge(described_recipe)
+
+it 'installs apache2' do 
+   expect(chef_run).to install_package('httpd')
+ end
+end
+
